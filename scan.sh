345a0a703e2397070c1a82d3a07f2c9724e9d3d6
Nasso Vikos <nas@vks.io>
2021-01-03 14:49:05 +0200

    Add services file as default port source

--- scan.sh
+++ scan.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 #
-# Usage: bash scan.sh --host <ip-address> --ports <port1,port2>
+# Usage: bash scan.sh --host <ip-address> [--ports <port1,port2>]
 # Author: Nasso Vikos <nas@vks.io>
 
-# Check that required parameters exist
-if [[ ! $1 ]] || [[ ! $2 ]] || [[ ! $3 ]] || [[ ! $4 ]]; then
-  echo "Usage: bash scan.sh --host <ip-address> --ports <port1,port2>"
+# Check which parameters exist
+if [[ $1 ]] && [[ $2 ]] && [[ ! $3 ]] && [[ ! $4 ]]; then
+  # Use /etc/services file as port list
+  if [[ -f /etc/services ]]; then
+    while read -r protocol port alias description; do
+      if [[ $port =~ "tcp" ]]; then
+        ports+=(${port/\/tcp})
+      else
+        continue
+      fi
+    done < /etc/services
+  else
+    echo "Error: Can't find file /etc/services."
+    exit 1
+  fi
+elif [[ $1 ]] && [[ $2 ]] && [[ $3 ]] && [[ $4 ]]; then
+  # Use --ports argument as port list
+  ports=(${4//,/ })
+else
+  echo "Usage: bash scan.sh --host <ip-address> [--ports <port1,port2>]"
   exit 1
 fi
 
 host=$2
-ports=(${4//,/ })
 
 function probe {
 ( pid=$BASHPID;
